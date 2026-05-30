e7e9bd6bf704e37e7a0af920ac4091ccb58683b5
anshverma1975 <162408906+anshverma1975@users.noreply.github.com>
2026-05-30 23:19:31 +0530

    minor changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
+import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <>
+      <Head>
+        <title>journal</title>
+        <meta
+          name="description"
+          content="a quiet place to write every day"
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
@@ -14,4 +27,4 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
       <SpeedInsights />
     </>
   );
-} 
+}
