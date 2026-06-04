b42b63c3fd1577e99ea0a05699efb67f35adbea5
anshverma1975 <162408906+anshverma1975@users.noreply.github.com>
2026-06-04 08:32:58 +0530

    added PWA

--- pages/_app.js
+++ pages/_app.js
@@ -4,25 +4,24 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <Head>
         <title>journal</title>
-        <meta
-          name="description"
-          content="a quiet place to write every day"
-        />
+        <meta name="description" content="a quiet place to write every day" />
+        <meta name="theme-color" content="#3d5a47" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="journal" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
       </Head>
-
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
-
       <Analytics />
       <SpeedInsights />
     </>
